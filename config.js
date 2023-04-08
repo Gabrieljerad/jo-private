95b700d6e2840a4eeabc6cb3824b9c246a7c727c
Gabrieljerad <gabrieljerad448@gmail.com>
2023-04-08 18:23:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -22,8 +22,8 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
+global.owner = ['254114035678','254106516012','254762065033']
+global.premium = ['254106516012']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
